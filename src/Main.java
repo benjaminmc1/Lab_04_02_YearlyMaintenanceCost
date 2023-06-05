6f1cb7fb214f91648d8308c75bc24d185e68066e
Benjamin <bmckee566@gmail.com>
2023-06-05 13:31:20 -0400

    Task 2 finish

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+        double fallMaintenance = 450.27;
+        double winterMaintenance = 306.44;
+        double springMaintenance = 549.59;
+        double summerMaintenance = 643.23;
+        double totalMaintenance;
+
+        totalMaintenance = fallMaintenance + winterMaintenance + springMaintenance + summerMaintenance;
+
+        System.out.println("The total maintenance costs for the year is " + totalMaintenance + " dollars.");
     }
 }
